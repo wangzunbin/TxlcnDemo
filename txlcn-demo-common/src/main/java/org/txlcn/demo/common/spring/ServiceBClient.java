37dd6104daef21fa094b51309f98022572d454de
wang <905192187@qq.com>
2019-09-24 16:55:12 +0800

    first commit

---
+++ txlcn-demo-common/src/main/java/org/txlcn/demo/common/spring/ServiceBClient.java
@@ -0,0 +1,19 @@
+package org.txlcn.demo.common.spring;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Description:
+ * Date: 2018/12/25
+ *
+ * @author ujued
+ */
+//@FeignClient(value = "txlcn-demo-spring-service-b", fallback = ServiceBFallback.class)
+@FeignClient(name = "service-b", url = "http://localhost:12002", fallback = ServiceBFallback.class)
+public interface ServiceBClient {
+
+    @GetMapping("/rpc")
+    String rpc(@RequestParam("value") String name);
+}
